77ffcfe77d1b4d8e17fcfa762a27d8e824930d25
artemflomaster <77832510+artemflomaster@users.noreply.github.com>
2022-11-18 18:15:44 +0300

    add sticky notes
    change links target
    build with new project

--- src/dataEn.js
+++ src/dataEn.js
@@ -1,6 +1,15 @@
 let dataEn;
 
 export default dataEn = [
+    {
+        id: 7,
+        section: 'Web',
+        title: 'Sticky Notes',
+        innertext: '<div>Web application with notes in React. </div> \n' +
+            '    <div>Some experiments with controlled form objects and WebKit CSS. Web data is saving in Local Storage</div> \n' +
+            '    <a href="sticky-notes" target="_blank"><img class="window" src="../screens/sticky-notes.jpg" width="650"  alt="Sticky Notes" /></a>',
+        tags: '["javascript", "React", "LocalStorage", "CSS", "figma"]'
+    },
 
 
     {
@@ -10,7 +19,7 @@ export default dataEn = [
         innertext: '<div>Web application build with React JS and prosesses data from simple API.</div> \n' +
             '    <div>The Trivia API Database <a href="https://opentdb.com/api_config.php" target="top">https://opentdb.com/api_config.php</a> </div> \n' +
             '    <div>Fetch, async, useEffect, States and a few callback hells</div> \n' +
-            '    <a href="quizzical" target="top"><img class="window" src="../screens/quizzical.jpg" width="650"  alt="Quizzical" /></a>',
+            '    <a href="quizzical" target="_blank"><img class="window" src="../screens/quizzical.jpg" width="650"  alt="Quizzical" /></a>',
         tags: '["javascript", "React", "API", "CSS", "web"]'
     },
     {
@@ -18,7 +27,7 @@ export default dataEn = [
         section: 'GameDev',
         title: 'Game for the weekend game jam',
         innertext: '<div>The project for game jam called One Button. Made in two evenings in Unity engine.</div>\n' +
-            '     <a href="https://artemflomaster.itch.io/one-button-two-colours" target="parent"><img class="border" src="../screens/one-button.jpg" alt="One Button" /></a>',
+            '     <a href="https://artemflomaster.itch.io/one-button-two-colours" target="_blank"><img class="border" src="../screens/one-button.jpg" alt="One Button" /></a>',
         tags: '["Csharp", "Unity", "GameDev"]'
     },
     {
@@ -26,7 +35,7 @@ export default dataEn = [
         section: 'GameDev',
         title: 'The Four Pillars of Object Oriented Programming',
         innertext: '<div>Game shows four pillars in the game as well is in the code</div>\n' +
-            '     <a href="https://play.unity.com/mg/other/webgl-builds-119289" target="parent"><img class="border" src="../screens/oop-game.jpg" alt="OOP pillars" /></a>',
+            '     <a href="https://play.unity.com/mg/other/webgl-builds-119289" target="_blank"><img class="border" src="../screens/oop-game.jpg" alt="OOP pillars" /></a>',
         tags: '["Csharp", "Unity", "GameDev"]'
     },
     {
@@ -34,7 +43,7 @@ export default dataEn = [
         section: 'GameDev',
         title: 'The Pachinko prototype',
         innertext: '<div>The Pachinko game prototype</div>\n' +
-            '     <a href="https://play.unity.com/mg/other/pachinko-counter" target="parent"><img class="border" src="../screens/pachinko.jpg" alt="Pachinko" /></a>',
+            '     <a href="https://play.unity.com/mg/other/pachinko-counter" target="_blank"><img class="border" src="../screens/pachinko.jpg" alt="Pachinko" /></a>',
         tags: '["Csharp", "Unity", "GameDev"]'
     },
     {
@@ -42,7 +51,7 @@ export default dataEn = [
         section: 'EtCetera',
         title: 'Èlka',
         innertext: '<div>New Year hobby project based in Arduino</div>\n' +
-            '     <a href="https://vimeo.com/manage/videos/56931288" target="parent"><img class="border" src="../screens/elka.gif" alt="Èlka" /></a>',
+            '     <a href="https://vimeo.com/manage/videos/56931288" target="_blank"><img class="border" src="../screens/elka.gif" alt="Èlka" /></a>',
         tags: '["Arduino", "Hardware"]'
     },
     {
@@ -51,8 +60,8 @@ export default dataEn = [
         title: 'Let me introduce myself',
         innertext: '<div>My name is Artem Alexandrov</div>\n' +
             '<div>I am passioned with programming and computer sciense.</div>\n' +
-            '<div>Please find my recent CV <a href="https://hh.ru/applicant/resumes/view?resume=0f2fc040ff0b1eb19d0039ed1f524237727646" alt="HeadHunter" target="parent">at hh.ru</a>' +
-            ' or open here <a href="../downloads/Artem_Alexandrov_cv_en_6-11-22.pdf" target="parent" alt="link to pdf">as pdf</a></div>'+
+            '<div>Please find my recent CV <a href="https://hh.ru/applicant/resumes/view?resume=0f2fc040ff0b1eb19d0039ed1f524237727646" alt="HeadHunter" target="_blank">at hh.ru</a>' +
+            ' or open here <a href="../downloads/Artem_Alexandrov_cv_en_6-11-22.pdf" target="_blank" alt="link to pdf">as pdf</a></div>'+
             '<img class="border" src="../screens/me.jpg" alt="me" />\n',
         tags: '["Programmer", "Engineer", "philanthropist"]'
     }
--- src/dataRu.js
+++ src/dataRu.js
@@ -1,6 +1,15 @@
 let dataRu;
 
 export default dataRu = [
+    {
+        id: 7,
+        section: 'Web',
+        title: 'Клейкие записки',
+        innertext: '<div>Приложение с записками сделанное на React. </div> \n' +
+            '    <div>Немного экспериментов с "контроллируемыми формами" и CSS. Данные сохраняются в Local Storage браузера.</div> \n' +
+            '    <a href="sticky-notes" target="_blank"><img class="window" src="../screens/sticky-notes.jpg" width="650"  alt="Sticky Notes" /></a>',
+        tags: '["javascript", "React", "LocalStorage", "CSS", "figma"]'
+    },
 
     {
         id: 1,
@@ -9,7 +18,7 @@ export default dataRu = [
         innertext: '<div>Web-приложение построено на основе React и перерабатывает данные API.</div> \n' +
             '    <div>База данных Trivia API <a href="https://opentdb.com/api_config.php" target="top">https://opentdb.com/api_config.php</a> </div> \n' +
             '    <div>Fetch, async, useEffect, States и парочка колбэкхеллов</div> \n' +
-            '    <a href="quizzical" target="top"><img class="window" src="../screens/quizzical.jpg" width="650"  alt="Quizzical" /></a>',
+            '    <a href="quizzical" target="_blank"><img class="window" src="../screens/quizzical.jpg" width="650"  alt="Quizzical" /></a>',
         tags: '["javascript", "React", "API", "CSS", "web"]'
     },
     {
@@ -18,7 +27,7 @@ export default dataRu = [
         title: 'Игра для геймджема "One Button"',
         innertext: '<div>Геймджем выходного дня с темой – "одна кнопка, два цвета". Сделано за пару вечеров в Unity.</div>\n' +
             '<div>На каждом уровне генерируется два цвета и ими раскрашиваются элементы по границе экрана. Задача – перекрасить все элементы в один цвет. С каждым уровнем добавляется усложняющий элемент на поле</div>\n' +
-            '     <a href="https://artemflomaster.itch.io/one-button-two-colours" target="parent"><img class="border" src="../screens/one-button.jpg" alt="One Button" /></a>',
+            '     <a href="https://artemflomaster.itch.io/one-button-two-colours" target="_blank"><img class="border" src="../screens/one-button.jpg" alt="One Button" /></a>',
         tags: '["Csharp", "Unity", "GameDev"]'
     },
     {
@@ -27,7 +36,7 @@ export default dataRu = [
         title: 'Четыре столпа ООП',
         innertext: '<div>В игре можно увидеть четыре столпа Объектно Ориентированного Программирования в прямом смысле и в коде.</div>\n' +
             '<div>Шутливая игра-тренировка в рамках отработки приципов ООП на C#</div>\n' +
-            '     <a href="https://play.unity.com/mg/other/webgl-builds-119289" target="parent"><img class="border" src="../screens/oop-game.jpg" alt="OOP pillars" /></a>',
+            '     <a href="https://play.unity.com/mg/other/webgl-builds-119289" target="_blank"><img class="border" src="../screens/oop-game.jpg" alt="OOP pillars" /></a>',
         tags: '["Csharp", "Unity", "GameDev"]'
     },
     {
@@ -35,7 +44,7 @@ export default dataRu = [
         section: 'GameDev',
         title: 'Игра Pachinko',
         innertext: '<div>Автоматы с игрой Pachinko популярны в Японии. Эта игра является вариацией на тему Pachinko на движке Unity</div>\n' +
-            '     <a href="https://play.unity.com/mg/other/pachinko-counter" target="parent"><img class="border" src="../screens/pachinko.jpg" alt="Pachinko" /></a>',
+            '     <a href="https://play.unity.com/mg/other/pachinko-counter" target="_blank"><img class="border" src="../screens/pachinko.jpg" alt="Pachinko" /></a>',
         tags: '["Csharp", "Unity", "GameDev"]'
     },
     {
@@ -44,7 +53,7 @@ export default dataRu = [
         title: 'Èlka',
         innertext: '<div>Новогодний проект на Arduino.</div>\n' +
             '<div>Использованы ультразвузовой дальномер, светодиоды и бипер от компьютера</div>\n' +
-            '     <a href="https://vimeo.com/manage/videos/56931288" target="parent"><img class="border" src="../screens/elka.gif" alt="Èlka" /></a>',
+            '     <a href="https://vimeo.com/manage/videos/56931288" target="_blank"><img class="border" src="../screens/elka.gif" alt="Èlka" /></a>',
         tags: '["Arduino", "Hardware"]'
     },
     {
@@ -53,8 +62,8 @@ export default dataRu = [
         title: 'Позвольте представиться',
         innertext: '<div>Меня зовут Артём Александров</div>\n' +
             '<div>Программирование это моя страсть и хобби</div>\n' +
-            '<div>Моё свежее резюме можно <a href="https://hh.ru/applicant/resumes/view?resume=0f2fc040ff0b1eb19d0039ed1f524237727646" alt="HeadHunter" target="parent">открыть на hh.ru</a>'+
-             ' или открыть <a href="../downloads/Александров Артём Петрович_6-11-22.pdf" target="parent" alt="ссылка на pdf">как pdf</a></div>' +
+            '<div>Моё свежее резюме можно <a href="https://hh.ru/applicant/resumes/view?resume=0f2fc040ff0b1eb19d0039ed1f524237727646" alt="HeadHunter" target="_blank">открыть на hh.ru</a>' +
+            ' или открыть <a href="../downloads/Александров Артём Петрович_6-11-22.pdf" target="_blank" alt="ссылка на pdf">как pdf</a></div>' +
             '<img class="border" src="../screens/me.jpg" alt="me" />\n',
         tags: '["Programmer", "Engineer", "philanthropist"]'
     }
